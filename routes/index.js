69f935f96320eaa67a2dcd538a3358ded9c501ea
syx <982296740@qq.com>
2018-04-19 17:36:39 +0800

    checkid

--- routes/index.js
+++ routes/index.js
@@ -1,72 +1,90 @@
 var express = require('express');
 var connection = require('./db');
-var crypto=require("crypto");
+var crypto = require("crypto");
 var router = express.Router();
 connection.connect();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
-  connection.query('insert into we(w) value(?)',[5],function (err, result) {
-    if(err){
-      console.log('[INSERT ERROR] - ',err.message);
+router.get('/', function (req, res, next) {
+
+  connection.query('insert into we(w) value(?)', [5], function (err, result) {
+    if (err) {
+      console.log('[INSERT ERROR] - ', err.message);
       return;
     }
-    
+
   });
-  
+
   res.render('index', { title: 'Express' });
 });
 
-function User(user){
-	this.id=user.id;//数据库中 _id
-	this.name=user.name;//用户名
-	this.password=user.password;//用户密码
-	this.veri=user.veri;//验证码
+function User(user) {
+  this.id = user.id;//数据库中 _id
+  this.name = user.name;//用户名
+  this.password = user.password;//用户密码
+  this.veri = user.veri;//验证码
 }
-router.get('/AdminLoginAndRegHandler', function(req, res, next) {
+router.get('/AdminLoginAndRegHandler', function (req, res, next) {
   //	req.query
-    switch (req.query.action) {
-      case "verification":
-        //获取验证码
-        var str = "qwertyuiopasdfghjklzxcvbnm1234567890";
-        var randomNum = function(min, max) {
-          return Math.floor(Math.random() * (max - min) + min);
-        }
-        var veri = "";
-        for (var i = 0; i < 4; i++) {
-          veri += str[randomNum(0, str.length)];
-        }
-        req.session.user = new User({
-          name: "",
-          password: "",
-          id: "",
-          veri: veri
-        })
+  switch (req.query.action) {
+    case "verification":
+      //获取验证码
+      var str = "qwertyuiopasdfghjklzxcvbnm1234567890";
+      var randomNum = function (min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+      }
+      var veri = "";
+      for (var i = 0; i < 4; i++) {
+        veri += str[randomNum(0, str.length)];
+      }
+      req.session.user = new User({
+        name: "",
+        password: "",
+        id: "",
+        veri: veri
+      })
+      res.send({
+        success: "成功",
+        data: veri
+      });
+
+      break;
+    case "checkVerification":
+      //校验验证码
+      var veri = req.query.veri;
+      var sessionVeri = req.session.user.veri;
+      if (veri == sessionVeri) {
         res.send({
-          success: "成功",
-          data: veri
+          success: "验证码正确"
         });
-  
-        break;
-      case "checkVerification":
-        //校验验证码
-        var veri = req.query.veri;
-        var sessionVeri = req.session.user.veri;
-        if (veri == sessionVeri) {
+      } else {
+        res.send({
+          err: "验证码错误"
+        });
+      }
+
+      break;
+    case "checkid":
+      var id=req.query.id
+      connection.query('select count(*) as v from user where id=?', [id], function (err, result) {
+        if (err) {
+          console.log('[INSERT ERROR] - ', err.message);
+          return;
+        }
+        if (result[0].v == 0) {
           res.send({
-            success: "验证码正确"
+            success: "id可用"
           });
         } else {
           res.send({
-            err: "验证码错误"
+            err: "id不可用"
           });
         }
-  
-        break;
-      default:
-        break;
-    }
-  
-  });
+      });
+      break;
+    default:
+      break;
+  }
+
+});
 
 module.exports = router;
