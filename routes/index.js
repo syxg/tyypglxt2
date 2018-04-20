3601b5658ee41e683d394a02ef4fce6579256e2b
syx <982296740@qq.com>
2018-04-20 16:16:24 +0800

    login

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,36 @@
 var express = require('express');
 var connection = require('./db');
-var crypto=require("crypto");
+var crypto = require("crypto");
 var router = express.Router();
 connection.connect();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
-  connection.query('insert into we(w) value(?)',[5],function (err, result) {
-    if(err){
-      console.log('[INSERT ERROR] - ',err.message);
+router.get('/', function (req, res, next) {
+
+  connection.query('insert into we(w) value(?)', [5], function (err, result) {
+    if (err) {
+      console.log('[INSERT ERROR] - ', err.message);
       return;
     }
-    
+
   });
-  
+
   res.render('index', { title: 'Express' });
 });
-router.post('/AdminLoginAndRegHandler',function(req, res, next){
-  switch(req.query.action){
+router.post('/AdminLoginAndRegHandler', function (req, res, next) {
+  switch (req.query.action) {
     case "register":
       var userInfos = {};
-      userInfos.id=req.body.id;
-      userInfos.name=req.body.name;
-      userInfos.sex=req.body.sex;
-      userInfos.phone= /^1\d{10}$/.test(parseInt(req.body.phone)) ? req.body.phone : null;
-      userInfos.email=req.body.email;
+      userInfos.id = req.body.id;
+      userInfos.name = req.body.name;
+      userInfos.sex = req.body.sex;
+      userInfos.phone = /^1\d{10}$/.test(parseInt(req.body.phone)) ? req.body.phone : null;
+      userInfos.email = req.body.email;
       var md5 = crypto.createHash("md5");
       userInfos.password = md5.update(req.body.password).digest("base64");
-      connection.query('insert into user(id,name,sex,phone,email,password) value(?,?,?,?,?,?)',[userInfos.id,userInfos.name,userInfos.sex,userInfos.phone,userInfos.email,userInfos.password],function (err, result) {
-        if(err){
-          console.log('[INSERT ERROR] - ',err.message);
-          res.send({ err: "注册失败:"+ err.message});
+      connection.query('insert into user(id,name,sex,phone,email,password) value(?,?,?,?,?,?)', [userInfos.id, userInfos.name, userInfos.sex, userInfos.phone, userInfos.email, userInfos.password], function (err, result) {
+        if (err) {
+          console.log('[INSERT ERROR] - ', err.message);
+          res.send({ err: "注册失败:" + err.message });
           return;
         }
         if (result.affectedRows == 1) {
@@ -39,6 +39,25 @@ router.post('/AdminLoginAndRegHandler',function(req, res, next){
           res.send({ err: "注册失败" });
         }
       });
+      break;
+    case "login":
+      var md5 = crypto.createHash("md5");
+      var password = md5.update(req.body.password).digest("base64");
+      connection.query('select count(*) as v from user where id=? and password=?', [req.body.id,password], function (err, result) {
+        if (err) {
+          console.log('[INSERT ERROR] - ', err.message);
+          res.send({ err: "登录失败:" + err.message });
+          return;
+        }
+        console.log(result[0].V) ;
+        if (result[0].V == 1) {
+          res.send({ success: "登录成功" });
+        } else {
+          res.send({ err: "登录失败" });
+        }
+      });
+      break;
+
 
   }
 });
